60a25220c46d4c1009eaf0fde8265e9b305fc68e
Nilesh <nilesh14890@gmail.com>
2023-12-05 22:29:42 +0530

    sidebar alignment

--- src/components/buttons/Hamburger.tsx
+++ src/components/buttons/Hamburger.tsx
@@ -1,7 +1,7 @@
 const Hamburger = () => {
-  const barClass = "w-5 h-0.5 bg-white";
+  const barClass = "h-[1px] bg-white";
   return (
-    <div className="cursor-pointer flex flex-col gap-1 w-5">
+    <div className="cursor-pointer flex flex-col gap-[5px] w-12 px-3 py-3 hover:bg-hover-bg rounded-[50%]">
       {[1, 2, 3].map((i: number) => (
         <div className={barClass} key={i}></div>
       ))}
